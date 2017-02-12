34d7e5f24d828d0fd65c015b1a581476d999eb05
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-02-12 16:19:32 +0100

    Toggle starting moving

--- pad.js
+++ pad.js
@@ -5,51 +5,58 @@ var keys = {
     right: false,
     moving: false
 };
+var started = false;
 function onLoad() {
     var isAndroid = /Android/i.test(navigator.userAgent);
     if (window.DeviceMotionEvent !== undefined) {
         window.ondevicemotion = function (event) {
-            ax = Math.floor(event.accelerationIncludingGravity.x);
-            ay = Math.floor(event.accelerationIncludingGravity.y);
-            az = Math.floor(event.accelerationIncludingGravity.z);
-            document.getElementById('x').innerHTML = ax;
-            document.getElementById('y').innerHTML = ay;
-            document.getElementById('z').innerHTML = az;
-            if (isAndroid ? ax > 4 : ax < -4) {
-                if (!keys.left) {
-                    keys.left = true;
-                    updateMoving();
+            if (started) {
+                ax = Math.floor(event.accelerationIncludingGravity.x);
+                ay = Math.floor(event.accelerationIncludingGravity.y);
+                az = Math.floor(event.accelerationIncludingGravity.z);
+                document.getElementById('x').innerHTML = ax;
+                document.getElementById('y').innerHTML = ay;
+                document.getElementById('z').innerHTML = az;
+                if (isAndroid ? ax > 4 : ax < -4) {
+                    if (!keys.left) {
+                        keys.left = true;
+                        updateMoving();
+                    }
+                } else if (isAndroid ? ax < -4 : ax > 4) {
+                    if (!keys.right) {
+                        keys.right = true;
+                        keys.left = false;
+                        updateMoving();
+                    }
+                } else {
+                    if (keys.moving) {
+                        keys.right = false;
+                        keys.left = false;
+                        updateMoving();
+                    }
                 }
-            } else if (isAndroid ? ax < -4 : ax > 4) {
-                if (!keys.right) {
-                    keys.right = true;
-                    keys.left = false;
-                    updateMoving();
-                }
-            } else {
-                if (keys.moving) {
-                    keys.right = false;
-                    keys.left = false;
-                    updateMoving();
-                }
-            }
-            if (isAndroid ? az > 4 : az < -4) {
-                if (!keys.forward) {
-                    keys.forward = true;
-                    keys.moving = true;
-                    document.getElementById('direction').innerHTML = 'FORWARD';
-                    updateMoving();
-                }
-            } else {
-                if (keys.forward) {
-                    keys.forward = false;
-                    keys.moving = false;
-                    document.getElementById('direction').innerHTML = 'STOP';
-                    updateMoving();
+                if (isAndroid ? az > 4 : az < -4) {
+                    if (!keys.forward) {
+                        keys.forward = true;
+                        keys.moving = true;
+                        document.getElementById('direction').innerHTML = 'FORWARD';
+                        updateMoving();
+                    }
+                } else {
+                    if (keys.forward) {
+                        keys.forward = false;
+                        keys.moving = false;
+                        document.getElementById('direction').innerHTML = 'STOP';
+                        updateMoving();
+                    }
                 }
             }
         }
     }
+    window.addEventListener("touchstart", function() {
+        started = !started;
+        document.body.style.backgroundColor = started ? 'blue' : 'black';
+    }, false);
 }
 function updateMoving() {
     firebase.database().ref('moving').set(keys);
